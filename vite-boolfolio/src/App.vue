f35c48261ab777cf39e42af3bf71c689c6ceaca3
TomasBelfonte <tomas.belfonte@gmail.com>
2023-02-18 21:56:25 +0100

    adjust nav

--- vite-boolfolio/src/App.vue
+++ vite-boolfolio/src/App.vue
@@ -14,6 +14,9 @@ export default {
     methods: {
       fetchProjects() {
         axios.get("http://127.0.0.1:8000/api/projects")
+        .then((resp) => {
+          this.projects = (resp.data);
+        })
       }
     },
 
@@ -26,22 +29,31 @@ export default {
 <template>
   <TheHeader></TheHeader>
 
-  <div class="container">
-    <h2>Lista Progetti</h2>
+  <div class="container text-center">
+    <h4>Lista Progetti</h4>
 
     <table class="table">
       <thead>
         <tr>
+          <th>Cover</th>
           <th>Titolo</th>
-          <th>Utente</th>
+          <th>Utente_ID</th>
           <th>Contenuto</th>
-          <th>Tecnologia</th>
+          <th>Status</th>
         </tr>
       </thead>
 
       <tbody>
-        <tr>
-
+        <tr v-for="project in projects" :key="project.it" >
+          <td>
+            <div v-if="project.cover_img">
+            <img src="http://127.0.0.1:8000/storage/img/the-lich-king.png" alt="" style="width: 60px">
+            </div>
+          </td>
+          <td>{{ project.title }}</td>
+          <td>{{ project.user_id }}</td>
+          <td>{{ project.content }}</td>
+          <td>{{ project.status }}</td>
         </tr>
       </tbody>
     </table>
