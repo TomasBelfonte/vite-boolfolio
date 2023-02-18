5edebc3db915da459e099bfda2b2dcb409fb8ced
TomasBelfonte <tomas.belfonte@gmail.com>
2023-02-18 19:37:39 +0100

    initial comit - structure -Header

---
+++ vite-boolfolio/src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import TheHeader from './components/TheHeader.vue';
+
+export default {
+    data() {
+        return {
+            "message": "ciao come va?"
+        };
+    },
+    components: { TheHeader }
+};
+</script>
+
+<template>
+  <TheHeader></TheHeader>
+<h1>fanc</h1>
+</template>
+
+<style lang="scss">
+@use "styles/main";
+</style>
---
+++ vite-boolfolio/src/components/TheHeader.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Progetto BootFolio</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item" v-for="(item, index) in menuItems" :key="index">
+                        <a class="nav-link" :href="item.index">{{ item.label }}</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                menuItems: [
+                    {
+                        label: "Home",
+                        routeName: "home",
+                    },
+                    {
+                        label:"Projects",
+                        routeName:"projects",
+                    },
+                    {
+                        label:"Chi siamo",
+                        routeName: "about",
+                    },
+                    {
+                        label:"Contatti",
+                        routeName:"contacts",
+                    },
+                ],
+            };
+            
+        },
+    };
+</script>
+
+<style scoped lang="scss">
+</style>
