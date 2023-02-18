026bb57b487026c1ef5653818325c44a38e14a60
TomasBelfonte <tomas.belfonte@gmail.com>
2023-02-18 19:56:29 +0100

    axios

--- vite-boolfolio/src/App.vue
+++ vite-boolfolio/src/App.vue
@@ -2,18 +2,51 @@
 import TheHeader from './components/TheHeader.vue';
 
 export default {
+    components: { TheHeader },
+
     data() {
         return {
-            "message": "ciao come va?"
+          projects: [],
         };
     },
-    components: { TheHeader }
+  
+    methods: {
+      fetchProjects() {
+        
+      }
+    },
+
+    mounted() {
+      this.fetchProjects();
+    }
 };
 </script>
 
 <template>
   <TheHeader></TheHeader>
-<h1>fanc</h1>
+
+  <div class="container">
+    <h2>Lista Progetti</h2>
+
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Titolo</th>
+          <th>Utente</th>
+          <th>Contenuto</th>
+          <th>Tecnologia</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr>
+
+        </tr>
+      </tbody>
+    </table>
+
+  </div>
+
 </template>
 
 <style lang="scss">
